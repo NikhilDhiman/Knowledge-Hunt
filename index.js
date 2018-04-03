3f4f7f310bdd90ef7797c32d496be03f2890084b
NikhilDhiman <nikhildhiman.ntpc@gmail.com>
2018-04-03 22:22:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $("li#l_header a").click(function(){
+        $("#login_box").fadeIn();
+    });
+	
+	$("i#login_box_c").click(function(){
+		$("#login_box").fadeOut();	
+	});
+	
+	$("li#s_header a").click(function(){
+        $("#signup_box").fadeIn();
+    });
+	
+	$("i#signup_box_c").click(function(){
+		$("#signup_box").fadeOut();	
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}); 
